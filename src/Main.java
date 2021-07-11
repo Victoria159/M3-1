60f146431e78c9134b7e74453b632bf9a686a921
Victoria Bocharova <v.s.bocha@gmail.com>
2021-07-11 16:44:17 +0300

    New number card

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4929869750422014";
+        String number = "4485729278756162468";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
