4876b032e6679e08fd0061e1f1bd4c20d521a019
Victoria Bocharova <v.s.bocha@gmail.com>
2021-07-11 16:43:19 +0300

    changet number card

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "30024525696252";
+        String number = "4929869750422014";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
